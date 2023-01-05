9f13fc01f71c1cbc32e2b46fbab1ace19c9584d6
Hubert <hubertwojnarowicz01@gmail.com>
2023-01-05 21:57:48 +0100

    add session provider + theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
+"use client";
 import { SkipNavLink } from "@reach/skip-nav";
 import "../styles/globals.css";
 import Header from "./header";
 import { Space_Grotesk } from "@next/font/google";
+import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
 
 const spaceGrotesk = Space_Grotesk({
-  variable: "--font-space-grotesk",
   subsets: ["latin"],
+  variable: "--font-space-grotesk",
 });
 
 export default function RootLayout({
@@ -15,11 +18,14 @@ export default function RootLayout({
 }) {
   return (
     <html className={`${spaceGrotesk.variable} font-sans`}>
-      <head />
-      <body className="bg-background">
-        <SkipNavLink className="absolute top-2 left-2 py-4 px-6 invisible h-0.5 w-0.5" />
-        <Header />
-        {children}
+      <body className="dark:bg-background dark:text-text text-background bg-text">
+        <SessionProvider>
+          <ThemeProvider attribute="class" defaultTheme="dark">
+            <SkipNavLink className="absolute top-2 left-2 py-4 px-6 invisible h-0.5 w-0.5" />
+            <Header />
+            {children}
+          </ThemeProvider>
+        </SessionProvider>
       </body>
     </html>
   );
